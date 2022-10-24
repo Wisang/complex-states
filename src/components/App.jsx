47ff204456c64441e854c8b8489454aa0f4d273d
wisang <wseom7@gmail.com>
2022-10-24 18:19:41 +0900

    my working version

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import useState from "react";
 
 function App() {
+  const [firstName, setFirstName] = React.useState("");
+  const [lastName, setLastName] = React.useState("");
+
+  function handleFirstName(e) {
+    setFirstName(e.target.value);
+  }
+
+  function handleLastName(e) {
+    setLastName(e.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello {firstName} {lastName}</h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input onChange={handleFirstName} name="fName" placeholder="First Name" />
+        <input onChange={handleLastName} name="lName" placeholder="Last Name" />
         <button>Submit</button>
       </form>
     </div>
